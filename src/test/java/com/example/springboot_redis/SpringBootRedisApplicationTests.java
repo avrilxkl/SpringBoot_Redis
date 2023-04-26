d3cdf376a42401b81eb74b85ab0e6d9b58ed79ff
avril <3106846488@qq.com>
2023-04-26 23:15:15 +0800

    main commit

--- src/test/java/com/example/springboot_redis/SpringBootRedisApplicationTests.java
+++ src/test/java/com/example/springboot_redis/SpringBootRedisApplicationTests.java
@@ -1,12 +1,7 @@
 package com.example.springboot_redis;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
-
-import javax.annotation.Resource;
 
 @SpringBootTest
 class SpringBootRedisApplicationTests {
